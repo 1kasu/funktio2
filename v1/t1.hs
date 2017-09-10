d0f4fbf7458998a376b1595057e9b5ae28fdfd4a
Kasimir Ilmonen <jyrysanteri@hotmail.com>
2017-09-10 20:44:59 +0300

    Sain kaantajan hyvaksymaan.

--- v1/t1.hs
+++ v1/t1.hs
@@ -4,10 +4,10 @@ newtype Count = Count {getCount :: Integer}
 
 -- b 
 
-newtype Sum a = Sum {getSum :: a} using the constraint Num a
+newtype Sum a = Sum {getSum :: a} --using the constraint Num a
 
 -- c
-newtype Product a = Product {getProduct :: a} using the constraint Num a
+newtype Product a = Product {getProduct :: a} --using the constraint Num a
 
 -- d
 newtype First a = First {getFirst :: Maybe a}
@@ -16,16 +16,16 @@ newtype First a = First {getFirst :: Maybe a}
 newtype Last a = Last {getLast :: Maybe a}
 
 -- f
-newtype Min a = Min {getMin :: a} using the constraint Ord a
+newtype Min a = Min {getMin :: a} --using the constraint Ord a
 
 -- g
-newtype Max a = Max {getMax :: a} using the constraint Ord a
+newtype Max a = Max {getMax :: a} --using the constraint Ord a
 
 -- h
 data NonEmpty a = a :| [a]
 
 -- i
-newtype MonMap k a = Mon (Map k a) using the constraint Monoid a with Map being from Data.Map
+--newtype MonMap k a = Mon (Map k a) using the constraint Monoid a with Map being from Data.Map
 
 -- j
 newtype Predicate a = Pred (a -> Bool)
@@ -34,13 +34,13 @@ newtype Predicate a = Pred (a -> Bool)
 newtype Endo a = Endo {appEndo :: a -> a}
 
 -- l
-a -> b using the constraint Monoid b
+--a -> b using the constraint Monoid b
 
 
 -- m
-(a, b) using the constraint (Monoid a, Monoid b)
+--(a, b) using the constraint (Monoid a, Monoid b)
 
 -- n
-IO a using the constraint Monoid a 
+--IO a using the constraint Monoid a 
 
 
